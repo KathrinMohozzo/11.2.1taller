a121d54562fb714b2f695bab9f9465ed6cd1f0dd
KathrinMohozzo <kathrinmohozo05@gmail.com>
2023-09-05 11:38:01 -0300

    Cambios finales

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 document.getElementById('btnsubmit').addEventListener('click', () => {
+    
     const nom = document.getElementById('name').value;
     const ap = document.getElementById('surname').value;
-    const fecha = document.getElementById('date').value;
+    const fechaDeNac = document.getElementById('date').value;
 
     const data = {
         name: nom,
         surname: ap,
-        fechaDeNacimiento: fecha //no esta en el endpoint igual
+        fechaDeNacimiento: fechaDeNac //no esta en el endpoint igual
     };
 
     fetch('https://jsonplaceholder.typicode.com/users', {
         method: 'POST',
         headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
         },
         body: JSON.stringify(data),
     })
-    .then(response => response.json())
-    .then(responseData => {
-        console.log("Success:", responseData);
+
+    .then(response => {
+        if (response.ok){
+            console.log('Los datos han sido enviados.');
+        } else {
+            console.error('Error al enviar datos.');
+        }
     })
+
     .catch(error => {
-        console.error("Error:", error);
+        console.error('Error al enviar datos.', error);
     });
 });
